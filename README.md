bff728ec56718e381e4041bad1f9cbb9c0360a48
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2022-09-11 17:11:10 +0200

    Code refactoring, added symbol_set.txt

--- README.md
+++ README.md
@@ -2,23 +2,38 @@
 ###### Another project guided/inspired by https://robertheaton.com/2018/06/12/programming-projects-for-advanced-beginners-ascii-art/ 
 ###### This time around it's a program that prints ASCII art in the terminal based on an image you give
 ---
-manual will be written when I've added more features! Currently it just opens `my_github_avatar.jpg` stored in the `images` folder and prints an ASCII art equivalent.
+manual:
+
+This program lets you select an image through your OS' file dialog and prints an ASCII_art equivalent in the terminal/console.
+You can change the symbols used to generate the art in `symbol_set.txt` found in the `resources` folder (left for "darker" spots and right for "brighter" spots). In case of an empty `symbol_set.txt`, or the file not being found, defaults to a hardcoded symbol set.
+This is important when you want to convert images with less color variety, like images of cartoons/anime.
+Currently resizes images with a pixel width higher than 100 down to a width of 100, retaining aspect ratio. Will add feature to change this value with command line args.
+
+---
+BUGS:
+Currently does not work with images that store transparency data or other data that's not just RBG values of each pixel (.png, .gif, etc).
 
 ---
 TODO:
 * Core feature
-- [x] Print ASCII art in the terminal based on the input image. Print an error message and panic!🦀 if the image can't be found.
+- [x] Print ASCII art in the terminal based on the input image. Print an error message and `panic!`🦀 if the image can't be found.
+* BUGFIXES
+- [ ] Account for image types that store more than just RGB values.
 * Usability/Customizability
 - [x] Make characters used modifyable
-	- [ ] Change default list to something more readable
-	- [ ] Let the user define a character list in a separate .txt file or command line args
+	- [x] Let the user define a character list in a separate `.txt` file
 - [x] Resize image automatically when exceeding a certain size
-	- [ ] Retain original aspect ratio of image
-		- [ ] Fix shifting of ASCII art when converting an image that's not 1:1 
+	- [x] Retain original aspect ratio of image
+		- [x] Fix shifting of ASCII art when converting an image that's not 1:1 
 	- [ ] Let the user define resize boundary through command line args
 - [x] Let the user select an image upon running the program, instead of hardcoding the image to be selected 
-	- [ ] Let program terminate elegantly when the user selects an incorrect file type or no file at all
+	- [ ] Replace instances of `panic!` with a message and a prompt asking if the user wants to select another image or terminate the program
 - [ ] Add multiple ways to recalculate brightness/color values
+	- [ ] Add brightness inversion option
+* Refactoring
+- [x] Wrap my code in separate functions
+- [x] Make all variables in `main()` unmutable (Did this actually make my code better/safer? No clue, but it was a fun exercise)
+- [x] Figure out how to not make the console window close immediately when running the executable
 
 Changelog:
 - 23/08/2022: 
@@ -30,3 +45,7 @@ Changelog:
 - 10/09/2022: 
 	- Added file dialog (should work cross platform).
 	- Added resizing functionality.
+- 11/09/2022:
+	- Refactored the code, fixed some bugs
+	- Added option for user to change symbols in symbol_set.txt
+	- Removed example image, since user can now select images manually
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,118 @@
 #![allow(non_snake_case)] // because the crate/binary name is not in snake case, and I want ASCII in all caps. :(
 
-use image::GenericImageView;
 use image::imageops::FilterType;
-use native_dialog::{FileDialog, MessageDialog, MessageType};
+use image::{DynamicImage, GenericImageView};
+use native_dialog::FileDialog;
+use std::fs;
+use std::io::stdin;
 
 fn main() {
-	// Open the image
+	// Open the image, and resize if the width goes beyond value given
+	// TODO: add command line arguments to change this value. currently always 100
+	let img = open_file_resize(100);
+	let dimensions = img.dimensions();
+
+	// use .into_bytes() to save original image color data in a Vec
+	// use value_calculation() to convert color data into something usable for ASCII art
+	let original_values = img.into_bytes();
+	// TODO: add different methods to calculate final value
+	let recalc_values = value_calculation(original_values);
+
+	// loads symbols stored in symbol_set.txt for use in the ASCII art
+	// in case the file has no symbols in it or the file cannot be found, uses a hardcoded default
+	let symbol_set = symbol_set_read();
+
+	// prints the characters to the terminal to generate ASCII art.
+	print_art(recalc_values, symbol_set, dimensions);
+	
+	// Waiting for user input, stop executable from closing instantly
+	pause_window();
+}
+
+fn open_file_resize(size_boundary: u32) -> DynamicImage {
 	let path = FileDialog::new()
-	.set_location("~/Desktop")
-	.show_open_single_file()
-	.unwrap();
+		.set_location("~/Desktop")
+		.show_open_single_file()
+		.expect("How'd you do that?");
 
+	// In case of no file being selected, panic!
 	let path = match path {
-        Some(path) => path,
-        None => return,
-    };
+		Some(path) => path,
+		None => panic!("select a file please"),
+	};
 
+	// panic! when selecting a non-image file
 	let mut img = match image::open(path) {
 		Ok(image) => image,
 		Err(error) => panic!("Problem opening file: {:?}", error),
 	};
 
-	// save dimensions and color values. Then convert them to a single value to generate ASCII art.
-	let mut dimensions = img.dimensions();
-	// resize image if it's dimensions exceed a certain boundary
-	if dimensions.0 > 30 {
-		img = img.resize(30, 30, FilterType::Gaussian);
-		dimensions = (30, 30);
+	// resize if width is higher than size_boundary. Otherwise do nothing.
+	resize(&mut img, size_boundary);
+
+	img
+}
+
+fn resize(img: &mut DynamicImage, target_size: u32) {
+	let dimensions = img.dimensions();
+	if dimensions.0 > target_size {
+		*img = img.resize(target_size, 10000, FilterType::Gaussian);
 	}
-	let byte_values: Vec<u8> = img.into_bytes();
+}
+
+fn value_calculation(original_values: Vec<u8>) -> Vec<u8> {
 	let mut recalc_values: Vec<u8> = Vec::new();
 	let mut count = 0;
-	// TODO: use a for loop to iterate over the correct elements instead of using count += 3.
-	// TODO: add multiple ways to calculate values for recalc_values
 
-	while count < byte_values.len() {
+	// Add up RGB values and divide by three
+	// TODO: learn how iterators work xd
+	while count < original_values.len() - 2 {
 		recalc_values.push(
-			((byte_values[count as usize] as u32
-				+ byte_values[count as usize + 1] as u32
-				+ byte_values[count as usize + 2] as u32)
+			((original_values[count as usize] as u32
+				+ original_values[count as usize + 1] as u32
+				+ original_values[count as usize + 2] as u32)
 				/ 3) as u8,
 		);
 		count += 3;
 	}
 
-	// Characters used to generate the art. I cannot index directly into a string like in C.
-	// So I put all the characters of this string into a Vec I can index into.
-	// You can modify this list without problem. As long as the length is not 0.
-	// TODO: read list from an external file
-	let char_list = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-	let mut char_list_vec: Vec<char> = Vec::new();
-	for characters in char_list.chars() {
-		char_list_vec.push(characters);
-	}
+	return recalc_values;
+}
 
-	// prints the characters to the terminal to generate ASCII art.
+fn symbol_set_read() -> Vec<char> {
+	let file_content_result = fs::read_to_string("./resources/symbol_set.txt");
+	// if file is empty or file cannot be found, defaults to hardcoded list
+	let file_content = match file_content_result {
+		Ok(content) => {
+			if content.len() > 0 {
+				content
+			} else {
+				"`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$".to_string()
+			}
+		}
+		_ => "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$".to_string(),
+	};
+	let mut symbol_set: Vec<char> = Vec::new();
+	for symbols in file_content.chars() {
+		symbol_set.push(symbols);
+	}
+	symbol_set
+}
+
+fn print_art(recalc_values: Vec<u8>, symbol_set: Vec<char>, dimensions: (u32, u32)) {
 	for (index, values) in recalc_values.iter().enumerate() {
 		// This calculation determines what character equivalent should be printed for a pixel value of 0-255
-		let character: usize = *values as usize * (char_list_vec.len() - 1) / 255;
+		let character: usize = *values as usize * (symbol_set.len() - 1) / 255;
 		if index % dimensions.0 as usize == 0 {
 			print!("\n");
 		}
-		print!("{}", char_list_vec[character])
+		print!("{}", symbol_set[character])
 	}
+}
+
+fn pause_window() {
+	let mut input_string = String::new();
+	println!("\nPress enter to close...");
+    stdin().read_line(&mut input_string)
+    	.ok();
 }
